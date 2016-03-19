9626fef3b3bef3a638f89ba7617b042667e3a47b
sizzly <rob.tacey@yahoo.com>
2016-03-19 12:07:50 -0400

    Commit 4.3-rc2: Improvements to modeling project pages and functionality

--- sites/all/modules/web_widgets/iframe/web_widgets_iframe.js
+++ sites/all/modules/web_widgets/iframe/web_widgets_iframe.js
@@ -6,34 +6,44 @@ if (document.getElementsByTagName && document.createElement && document.createTe
     wid: widgetContext['widgetid'],
     width: widgetContext['width'],
     height: widgetContext['height'],
-    scrolling: widgetContext['scrolling']
+    scrolling: widgetContext['scrolling'],
+    autoheight: widgetContext['autoheight'],
+    autoheight_method: widgetContext['autoheight_method']
   };
 
   for (var i in DrupalEmbed) {
     if (!DrupalEmbed[i]['processed']) {
       DrupalEmbed[i]['processed'] = true;
-      
+
       var separator = DrupalEmbed[i].src.indexOf('?') == -1 ? '?' : '&';
-      
+
       if (undefined===window.widget_count){
         window.widget_count = 0;
       }
       else {
         widget_count++;
       }
-      
+
       var script = document.getElementById(DrupalEmbed[i].wid);
       script.setAttribute('id', DrupalEmbed[i].wid);
-      
+
       var iframe = document.createElement('iframe');
       iframe.setAttribute('id', 'widgets-' + widget_count);
       iframe.setAttribute('frameBorder', '0');
-      iframe.setAttribute('width', DrupalEmbed[i].width);
-      iframe.setAttribute('height', DrupalEmbed[i].height);
+      if (DrupalEmbed[i].autoheight) {
+        iframe.setAttribute('width', '100%');
+      }
+      else {
+        iframe.setAttribute('height', DrupalEmbed[i].height);
+        iframe.setAttribute('width', DrupalEmbed[i].width);
+      }
       iframe.setAttribute('src', DrupalEmbed[i].src);
       iframe.setAttribute('scrolling', DrupalEmbed[i].scrolling);
-      
+
       script.parentNode.insertBefore(iframe, script);
+      if (DrupalEmbed[i].autoheight && (typeof window.iFrameResize == 'function')) {
+        iFrameResize({checkOrigin:false, heightCalculationMethod: DrupalEmbed[i].autoheight_method});
+      }
     }
   }
 }
--- sites/all/modules/web_widgets/iframe/web_widgets_iframe.tpl.php
+++ sites/all/modules/web_widgets/iframe/web_widgets_iframe.tpl.php
@@ -7,4 +7,7 @@
 <script type="text/javascript">
 widgetContext = <?php print $js_variables ?>;
 </script>
+<?php if (!empty($script_resize_url)): ?>
+  <script id="<?php print $wid ?>-resize" src="<?php print $script_resize_url ?>"></script>
+<?php endif ?>
 <script id="<?php print $wid ?>" src="<?php print $script_url ?>"></script>
